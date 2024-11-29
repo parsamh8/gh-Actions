dc5c421c273e6b7a2d03697bfa5449689826e096
Parsa Mh <157930237+parsamh8@users.noreply.github.com>
2024-11-29 17:12:59 -0500

    updates advertisment component

--- src/components/Advertisement.tsx
+++ src/components/Advertisement.tsx
@@ -7,6 +7,7 @@ interface AdvertisementProps {
 function Advertisement(props: AdvertisementProps) {
   return (
     <div className="advertisement">
+      <p>While supply last!!!</p>
       <p>{props.companyName}</p>
       <p>{props.productDescription}</p>
       <p>{props.price}</p>
